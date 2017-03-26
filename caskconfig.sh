73b83a0e87001f52887dba35fca7645f9f63f8e1
T0mmyKn1fe <theriddle53@gmail.com>
2017-03-27 10:57:30 +1100

    Initial commit.

---
+++ caskconfig.sh
@@ -0,0 +1,119 @@
+#!/bin/sh
+# relevant as of March 2017
+# make sure you chmod a+x on the file before running it 
+# 
+# Standard brew packages that I use alot.
+#
+brew cask install skype 
+brew cask install handbrake
+brew cask install plex-media-server 
+brew cask install picasa 
+brew cask install spotify 
+brew cask install utorrent 
+brew cask install the-unarchiver
+brew cask install virtualbox
+brew install git
+brew install Caskroom/versions/flux-beta
+brew install Caskroom/cask/spectacle
+brew install evernote
+brew install Caskroom/cask/google-drive
+brew install Caskroom/cask/google-chrome
+brew install Caskroom/cask/google-earth
+brew install firefox
+brew install flux-beta
+brew install node
+brew install java
+brew install Caskroom/versions/java7
+brew install Caskroom/cask/vagrant
+brew install Caskroom/cask/vagrant-manager
+brew install virtualboxbrew upd
+brew install ruby
+brew cask install vlc
+brew cask install gimp
+brew cask install Alfred
+brew install wget
+brew install Caskroom/versions/iterm2-beta
+brew install awscli
+brew install docker
+brew install Caskroom/cask/cyberduck
+brew install htop
+brew install visual-studio-code
+brew install zsh
+brew cask install wireshark
+brew cask install bartender
+brew cask install hyperdock
+brew cask install xtrafinder
+brew cask install cleanmymac
+brew install Caskroom/cask/1password
+brew install ansible
+brew cask install daisydisk
+brew install terraform-provisioner-ansible
+brew cask install parallels-desktop
+brew cask install virtualbox
+brew cask install virtualbox-extension-pack
+brew cask install adobe-reader
+brew cask install osxfuse
+brew cask install paragon-ntfs
+brew cask install flash-player
+brew cask install radio-silence
+brew cask install istat-menus
+brew cask install caffieine
+#
+# Network security and cracking tools.
+#
+brew cask install avg-antivirus
+brew install aircrack-ng
+brew install afl-fuzz
+brew install aircrack-ng
+brew install amap
+brew install argus
+brew install arp-scan 
+brew install arping 
+brew install binutils 
+brew install binwalk
+brew install bro
+brew install capstone
+brew install crunch
+brew install ettercap
+brew install hachoir
+brew install hping
+brew install ideviceinstaller 
+brew install ike-scan
+brew cask install istumbler
+brew install ipv6toolkit
+brew install john 
+brew install lft
+brew install libdnet
+brew install libimobiledevice
+brew install libnet 
+brew install littlesnitch
+brew install masscan 
+brew install net-snmp 
+brew install netcat 
+brew install nikto 
+brew install nmap 
+brew install netspot
+brew install openssl 
+brew install ophcrack 
+brew install p0f 
+brew install postgresql 
+brew install pwnat 
+brew install pwntools 
+brew install radare2 
+brew install reaver 
+brew install ruby 
+brew install sipsak 
+brew install skipfish 
+brew install sleuthkit 
+brew install snort 
+brew install socat 
+brew install sqlmap
+brew install ssdeep 
+brew install ssldump 
+brew install stunnel 
+brew install theharvester 
+brew install usbmuxd 
+brew install volatility
+brew cask install xquartz 
+brew install wireshark 
+brew install zmap
